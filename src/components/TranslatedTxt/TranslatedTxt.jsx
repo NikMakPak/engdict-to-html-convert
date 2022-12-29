b59f1dafaa729d6ff34f3830e009334e9cd524bd
nikmakpak <nicement28@gmail.com>
2022-12-29 13:06:17 +0300

    feat: add localstorage hook feat: implemented child-parent component binding

---
+++ src/components/TranslatedTxt/TranslatedTxt.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TranslatedTxt = ({text}) => {
+    return (
+        <div className={"translated font"}>
+            > <span>{text}</span>
+        </div>
+    );
+};
+
+export default TranslatedTxt;
